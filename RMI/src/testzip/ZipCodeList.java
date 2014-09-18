ea6d17bee01174d647c5e19131df88e664f8c3e1
zhengxiz <zxzhang1991@gmail.com>
2014-09-18 18:32:37 -0400

    Prof. Honda's java code. For the original version of this lab.

---
+++ RMI/src/testzip/ZipCodeList.java
@@ -0,0 +1,15 @@
+package testzip;
+
+public class ZipCodeList
+{
+    String city;
+    String ZipCode;
+    ZipCodeList next;
+
+    public ZipCodeList(String c, String z, ZipCodeList n)
+    {
+	city=c;
+	ZipCode=z;
+	next=n;
+    }
+}
---
+++ RMI/src/testzip/ZipCodeServer.java
@@ -0,0 +1,10 @@
+package testzip;
+
+public interface ZipCodeServer // extends YourRemote or whatever
+{
+    public void initialise(ZipCodeList newlist);
+    public String find(String city);
+    public ZipCodeList findAll();
+    public void printAll();
+}
+
